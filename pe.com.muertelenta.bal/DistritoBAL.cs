b8de51843cbf4adb16a2d4768a4d9cfdcebfcf1b
Francisco Huapaya <franciscojho@hotmail.com>
2025-05-27 23:07:41 +0200

    feat: add distrito classes

---
+++ pe.com.muertelenta.bal/DistritoBAL.cs
@@ -0,0 +1,30 @@
+﻿using pe.com.muertelenta.bo;
+using pe.com.muertelenta.dal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pe.com.muertelenta.bal
+{
+    public class DistritoBAL
+    {
+        private DistritoDAL distritoDAL = new DistritoDAL();
+
+        public List<DistritoBO> ShowDistrito()
+        {
+            return distritoDAL.ShowDistrito();
+        }
+
+        public List<DistritoBO> ShowAllDistrito()
+        {
+            return distritoDAL.ShowAllDistrito();
+        }
+
+        public bool addDistrito(DistritoBO distritoBO)
+        {
+            return distritoDAL.addDistrito(distritoBO);
+        }
+    }
+}
---
+++ pe.com.muertelenta.bo/DistritoBO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pe.com.muertelenta.bo
+{
+    public class DistritoBO
+    {
+        public int code { get; set; }
+        public string name { get; set; }
+        public bool state { get; set; }
+    }
+}
