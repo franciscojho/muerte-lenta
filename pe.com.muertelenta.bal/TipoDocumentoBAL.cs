13fb62cffd4ce4005d83881d7425f6e4fa741fbe
Francisco Huapaya <franciscojho@hotmail.com>
2025-05-28 22:06:32 +0200

    feat: add tipodocumento classes

---
+++ pe.com.muertelenta.bal/TipoDocumentoBAL.cs
@@ -0,0 +1,30 @@
+﻿using pe.com.muertelenta.bo;
+using pe.com.muertelenta.dal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pe.com.muertelenta.bal
+{
+    public class TipoDocumentoBAL
+    {
+        private TipoDocumentoDAL tipoDocumentoDAL = new TipoDocumentoDAL();
+
+        public List<TipoDocumentoBO> ShowTipoDocumento()
+        {
+            return tipoDocumentoDAL.ShowTipoDocumento();
+        }
+
+        public List<TipoDocumentoBO> ShowAllTipoDocumento()
+        {
+            return tipoDocumentoDAL.ShowAllTipoDocumento();
+        }
+
+        public bool addTipoDocumento(TipoDocumentoBO tipoDocumentoBO)
+        {
+            return tipoDocumentoDAL.addTipoDocumento(tipoDocumentoBO);
+        }
+    }
+}
