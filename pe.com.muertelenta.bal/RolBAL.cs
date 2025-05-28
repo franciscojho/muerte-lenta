c3b2f7b6eb2257e7e6962fe1204720e3de5e6039
Francisco Huapaya <franciscojho@hotmail.com>
2025-05-28 23:28:22 +0200

    feat: add rol classes

---
+++ pe.com.muertelenta.bal/RolBAL.cs
@@ -0,0 +1,29 @@
+﻿using pe.com.muertelenta.bo;
+using pe.com.muertelenta.dal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pe.com.muertelenta.bal
+{
+    public class RolBAL
+    {
+        private RolDAL rolDAL = new RolDAL();
+
+        public List<RolBO> ShowRol()
+        {
+            return rolDAL.ShowRol();
+        }
+
+        public List<RolBO> ShowAllRol()
+        {
+            return rolDAL.ShowAllRol();
+        }
+        public bool addRol(RolBO rolBO)
+        {
+            return rolDAL.addRol(rolBO);
+        }
+    }
+}
---
+++ pe.com.muertelenta.dal/RolDAL.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.Data.SqlClient;
+using pe.com.muertelenta.bo;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pe.com.muertelenta.dal
+{
+    public class RolDAL
+    {
+        private ConnectionDAL connection = new ConnectionDAL();
+        private SqlCommand sqlCommand;
+        private SqlDataReader reader;
+
+        public List<RolBO> ShowRol()
+        {
+            List<RolBO> rolBOs = new List<RolBO>();
+            try
+            {
+                sqlCommand = new SqlCommand();
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.CommandText = "SP_MostrarRol";
+                sqlCommand.Connection = connection.connect();
+                reader = sqlCommand.ExecuteReader();
+                while (reader.Read())
+                {
+                    RolBO rol= new RolBO();
+                    rol.code = Convert.ToInt32(reader["codrol"].ToString());
+                    rol.name = reader["nomrol"].ToString();
+                    rol.state = Convert.ToBoolean(reader["estrol"].ToString());
+                    rolBOs.Add(rol);
+                }
+                return rolBOs;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return null;
+            }
+            finally
+            {
+                connection.closeConnection();
+            }
+        }
+
+        public List<RolBO> ShowAllRol()
+        {
+            List<RolBO> rolBOs = new List<RolBO>();
+            try
+            {
+                sqlCommand = new SqlCommand();
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.CommandText = "SP_MostrarTodoRol";
+                sqlCommand.Connection = connection.connect();
+                reader = sqlCommand.ExecuteReader();
+                while (reader.Read())
+                {
+                    RolBO rol = new RolBO();
+                    rol.code = Convert.ToInt32(reader["codrol"].ToString());
+                    rol.name = reader["nomrol"].ToString();
+                    rol.state = Convert.ToBoolean(reader["estrol"].ToString());
+                    rolBOs.Add(rol);
+                }
+                return rolBOs;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return null;
+            }
+            finally
+            {
+                connection.closeConnection();
+            }
+        }
+
+        public bool addRol(RolBO rol)
+        {
+            int responseCode = 0;
+            try
+            {
+                sqlCommand = new SqlCommand();
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.CommandText = "SP_RegistrarRol";
+                sqlCommand.Connection = connection.connect();
+                sqlCommand.Parameters.AddWithValue("@nombre", rol.name);
+                sqlCommand.Parameters.AddWithValue("@estado", rol.state);
+                responseCode = sqlCommand.ExecuteNonQuery();
+                if (responseCode == 1)
+                {
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return false;
+            }
+            finally
+            {
+                connection.closeConnection();
+            }
+        }
+    }
+}
