cacd616c697ce3334892947f56fa863c859519eb
Francisco Huapaya <franciscojho@hotmail.com>
2025-06-11 21:36:04 +0200

    feat: add lesson 10

--- pe.com.muertelenta.bo/PlatoBO.cs
+++ pe.com.muertelenta.bo/PlatoBO.cs
@@ -9,8 +9,8 @@ namespace pe.com.muertelenta.bo
     public class PlatoBO
     {
         public int code { get; set; }
-        public int name { get; set; }
-        public int description { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
         public double price { get; set; }
         public int quantity { get; set; }
         public bool state { get; set; }
