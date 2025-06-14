daa2e588868f87fd0138dbf691a2c5e48dfda372
Francisco Huapaya <franciscojho@hotmail.com>
2025-06-14 20:03:18 +0200

    feat: improve tipodocumento form

--- pe.com.muertelenta.dal/TipoDocumentoDAL.cs
+++ pe.com.muertelenta.dal/TipoDocumentoDAL.cs
@@ -1,109 +1,69 @@
-﻿using Microsoft.Data.SqlClient;
-using pe.com.muertelenta.bo;
-using System;
-using System.Collections.Generic;
+﻿using pe.com.muertelenta.bo;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace pe.com.muertelenta.dal
 {
     public class TipoDocumentoDAL
     {
-        private ConnectionDAL connection = new ConnectionDAL();
-        private SqlCommand sqlCommand;
-        private SqlDataReader reader;
+        private SetupLookupItemDAL ProviderSetup = new SetupLookupItemDAL();
+        private LookupItemDAL LookupProvider;
+
+        public TipoDocumentoDAL()
+        {
+            ProviderSetup.GetLookupItemsCommandText = "SP_MostrartTipoDocumento";
+            ProviderSetup.GetAllLookupItemsCommandText = "SP_MostrarTipoDocumentoTodo";
+            ProviderSetup.AddLookupItemCommandText = "SP_RegistrarTipoDocumento";
+            ProviderSetup.UpdateLookupItemCommandText = "SP_ActualizarTipoDocumento";
+            ProviderSetup.EnableLookupItemCommandText = "SP_HabilitarTipoDocumento";
+            ProviderSetup.DeleteLookupItemCommandText = "SP_EliminarTipoDocumento";
+            ProviderSetup.DbSuffix = "tipd";
+            LookupProvider = new LookupItemDAL(ProviderSetup);
+        }
 
         public List<TipoDocumentoBO> ShowTipoDocumento()
         {
-            List<TipoDocumentoBO> tipòDocumentoBOs= new List<TipoDocumentoBO>();
-            try
+            List<LookupItemBO> items = LookupProvider.GetLookupItems();
+            List<TipoDocumentoBO> documentTypes = items.Select(item => new TipoDocumentoBO
             {
-                sqlCommand = new SqlCommand();
-                sqlCommand.CommandType = CommandType.StoredProcedure;
-                sqlCommand.CommandText = "SP_MostrartTipoDocumento";
-                sqlCommand.Connection = connection.connect();
-                reader = sqlCommand.ExecuteReader();
-                while (reader.Read())
-                {
-                    TipoDocumentoBO tipoDocumento = new TipoDocumentoBO();
-                    tipoDocumento.code = Convert.ToInt32(reader["codtipd"].ToString());
-                    tipoDocumento.name = reader["nomtipd"].ToString();
-                    tipoDocumento.state = Convert.ToBoolean(reader["esttipd"].ToString());
-                    tipòDocumentoBOs.Add(tipoDocumento);
-                }
-                return tipòDocumentoBOs;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-                return null;
-            }
-            finally
-            {
-                connection.closeConnection();
-            }
+                code = item.code,
+                name = item.name,
+                state = item.state
+            }).ToList();
+
+            return documentTypes;
         }
 
         public List<TipoDocumentoBO> ShowAllTipoDocumento()
         {
-            List<TipoDocumentoBO> tipòDocumentoBOs = new List<TipoDocumentoBO>();
-            try
+            List<LookupItemBO> items = LookupProvider.GetAllLookupItems();
+            List<TipoDocumentoBO> documentTypes = items.Select(item => new TipoDocumentoBO
             {
-                sqlCommand = new SqlCommand();
-                sqlCommand.CommandType = CommandType.StoredProcedure;
-                sqlCommand.CommandText = "SP_MostrartTodoTipoDocumento";
-                sqlCommand.Connection = connection.connect();
-                reader = sqlCommand.ExecuteReader();
-                while (reader.Read())
-                {
-                    TipoDocumentoBO tipoDocumento = new TipoDocumentoBO();
-                    tipoDocumento.code = Convert.ToInt32(reader["codtipd"].ToString());
-                    tipoDocumento.name = reader["nomtipd"].ToString();
-                    tipoDocumento.state = Convert.ToBoolean(reader["esttipd"].ToString());
-                    tipòDocumentoBOs.Add(tipoDocumento);
-                }
-                return tipòDocumentoBOs;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-                return null;
-            }
-            finally
-            {
-                connection.closeConnection();
-            }
+                code = item.code,
+                name = item.name,
+                state = item.state
+            }).ToList();
+
+            return documentTypes;
         }
 
-        public bool AddTipoDocumento(TipoDocumentoBO tipoDocumentoBO)
+        public bool AddTipoDocumento(TipoDocumentoBO documentType)
         {
-            int responseCode = 0;
-            try
-            {
-                sqlCommand = new SqlCommand();
-                sqlCommand.CommandType = CommandType.StoredProcedure;
-                sqlCommand.CommandText = "SP_RegistrarTipoDocumento";
-                sqlCommand.Connection = connection.connect();
-                sqlCommand.Parameters.AddWithValue("@nombre", tipoDocumentoBO.name);
-                sqlCommand.Parameters.AddWithValue("@estado", tipoDocumentoBO.state);
-                responseCode = sqlCommand.ExecuteNonQuery();
-                if (responseCode == 1)
-                {
-                    return true;
-                }
-                return false;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-                return false;
-            }
-            finally
-            {
-                connection.closeConnection();
-            }
+            return LookupProvider.AddLookupItem(documentType);
+        }
+
+        public bool UpdateTipoDocumento(TipoDocumentoBO documentType)
+        {
+            return LookupProvider.UpdateLookupItem(documentType);
+        }
+
+        public bool EnableTipoDocumento(TipoDocumentoBO documentType)
+        {
+            return LookupProvider.EnableLookupItem(documentType);
+        }
+
+        public bool DeleteTipoDocumento(TipoDocumentoBO documentType)
+        {
+            return LookupProvider.DeleteLookupItem(documentType);
         }
     }
 }
